237e6b65df0d95f9599dc927fa2eb978a3f2fe50
Tayme01 <temi.oluseun@yahoo.com>
2017-09-24 18:44:46 +0100

    My More Recipes Website

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$("#loginID").click(function(event){
+	$("#loginModal").modal('show');
+});
+
+$("#signUpBtn, #signupID").click(function(event){
+	$("#signupModal").modal('show');
+});
+
+$("#loginID").click(function(event){
+	$("#loginModal").modal('show');
+});
